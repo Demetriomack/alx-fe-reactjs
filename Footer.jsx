b118e5744f0ab6ec9bbee233acdc89a49909db7c
Demetriomack <123373569+Demetriomack@users.noreply.github.com>
2024-11-07 20:31:40 +0200

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,8 @@
+// src/components/Footer.jsx
+import React from 'react';
+
+const Footer = () => {
+  return <footer><p>&copy; 2024 My App. All rights reserved.</p></footer>;
+};
+
+export default Footer;
---
+++ MainContent.jsx
@@ -0,0 +1,8 @@
+// src/components/MainContent.jsx
+import React from 'react';
+
+const MainContent = () => {
+  return <main><p>This is the main content of the application.</p></main>;
+};
+
+export default MainContent;
